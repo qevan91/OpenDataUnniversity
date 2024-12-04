7d9b87e52fecc5e8eb46841bf958deb96eb185c1
ijayanra <jayanraj.ilangovane@ynov.com>
2024-12-04 16:48:31 +0100

    borne de recharge par region

--- nombre_de_bornes_de_charge.py
+++ nombre_de_bornes_de_charge.py
@@ -3,18 +3,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Charger les données
-data = pd.read_csv('consolidation-etalab-schema.csv', on_bad_lines='skip', sep=',', quotechar='"', engine='python')
-
-
-# Supprimer les duplicatas basés sur la colonne 'adresse_station'
-data_unique = data.drop_duplicates(subset='adresse_station')
+data = pd.read_csv('bornes-irve.csv', on_bad_lines='skip', sep=';', quotechar='"', engine='python')
 
 # Grouper les données par 'adresse_station' et sommer 'nbre_pdc'
-grouped_data = data_unique.groupby('adresse_station')['nbre_pdc'].sum().reset_index()
+grouped_data = data.groupby('Région')['nbre_pdc'].sum().reset_index()
 
 # Créer le graphique
 plt.figure(figsize=(12, 6))
-sns.lineplot(x='adresse_station', y='nbre_pdc', data=grouped_data)
+sns.barplot(x='Région', y='nbre_pdc', data=grouped_data)
 plt.title("Nombre de bornes de recharges")
 plt.xlabel("Lieux")
 plt.ylabel("Nombres de bornes")
